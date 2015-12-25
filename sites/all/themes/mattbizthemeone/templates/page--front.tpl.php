acfe298d5935bcb9a3fbba94ea7433541979c539
padmanabhan-developer <padmanabhan.developer.email@gmail.com>
2015-12-25 22:46:01 +0530

    codebase v0.1
    a draft version of mattbiz

---
+++ sites/all/themes/mattbizthemeone/templates/page--front.tpl.php
@@ -0,0 +1,9 @@
+<div class="homepage_wrapper">
+  <div class="vide_block" data-vide-bg="video/ocean" data-vide-options="position: 0% 50%">
+
+  </div>
+  <div class="footer">
+
+  </div>
+</div>
+<script src="/sites/all/themes/mattbizthemeone/scripts/jquery.vide.js"></script>
